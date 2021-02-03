3f44974300f6970d3853d5a4a1dd8cbe94913a69
moises_df <moisesintec@gmail.com>
2021-02-02 21:59:52 -0400

    Se agrego el form

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,17 @@
 from django import forms
 
-#class ContactForm(forms.Form):
+class ContactForm(forms.Form):
+    nombre = forms.CharField(label="Nombre",required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control','placeholder': 'Moises Diaz'}))
+
+    telefono = forms.CharField(label="Telefono",required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control','placeholder': '809-123-4567'}))
+
+    correo = forms.EmailField(label="Correo",required=True, widget=forms.EmailInput(
+        attrs={'class': 'form-control','placeholder': 'micorreo@gmail.com'}))
+
+    mensaje = forms.CharField(label="Mensaje",required=True,widget=forms.Textarea(
+        attrs={'class': 'form-control','placeholder': 'Me interesa ver esta propiedad','rows': '2'}))
+
+
     
--- core/views.py
+++ core/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.urls import reverse
 from portfolio.models import Inmueble
 from agents.models import Agent
+from contact.forms import ContactForm
 
 # Create your views here.
 def home(request):
     return render(request,"core/index.html")
 
 def portfolio(request,portfolio_id):
+
     inmueble = Inmueble.objects.get(id=portfolio_id) #Inmueble.objects.all().filter(id=portfolio_id)
     agent = Agent.objects.get(id=inmueble.agente_id)
-    return render(request,"core/index.html",{"inmueble":inmueble,"agent":agent})
+    contact_form = ContactForm()
+
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST) #Si el formulario fue enviado lo llenamos
+
+        if contact_form.is_valid():
+            nombre = request.POST.get("nombre",'')
+            telefono = request.POST.get("telefono",'')
+            correo = request.POST.get("correo",'')
+            mensaje = request.POST.get("mensaje",'')
+            #Si todo salio bien enviamos un mensaje
+            return redirect(f'/portfolio/{portfolio_id}/?ok')
+
+    return render(request,"core/index.html",{"inmueble":inmueble,"agent":agent,"form":contact_form})
 
