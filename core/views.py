5b542ca4a9d1eeade23ae1d342e293f48868b844
moises_df <moisesintec@gmail.com>
2021-03-24 15:48:28 -0400

    se agrego scraper.py 2

--- core/views.py
+++ core/views.py
@@ -67,12 +67,13 @@ def prueba(request):
     #url = "https://www.corotos.com.do/sc/inmuebles-en-venta/casas"
     url = "https://www.corotos.com.do/sc/inmuebles-en-venta/apartamentos"
     tipo_neg = "Venta"
-    driver = "core/chromedriver_88.exe"
+    driver = os.getcwd()+"/chromedriver_88.exe"
     solo_agentes = 0
     no_dupli = 1
+    resp = os.getcwd()
 
     #Vamos a llamar esto n veces teniendo en cuenta que siempre se para solo
     for n in range(1):
         feats = scrape_data_corotos(url,tipo_neg,2,driver,loads=0,solo_agentes=solo_agentes,no_dupli=no_dupli)
 
-    return render(request,"core/prueba.html")
+    return render(request,"core/prueba.html",{"resp":resp})
