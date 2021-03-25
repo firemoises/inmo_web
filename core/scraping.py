f477737f8390ff3c10bb4ea0ad8dcc8e1731f8c0
moises_df <moisesintec@gmail.com>
2021-03-25 15:34:52 -0400

    se agrego scraping.py

---
+++ core/scraping.py
@@ -0,0 +1,20 @@
+from core.scraper import scrape_data_corotos 
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+with Display():
+
+    #url = "https://www.corotos.com.do/sc/inmuebles-en-venta/casas"
+    url = "https://www.corotos.com.do/sc/inmuebles-en-venta/apartamentos"
+    tipo_neg = "Venta"
+    #driver = os.getcwd()+"/chromedriver_88.exe"
+    driver = os.getcwd()+"/geckodriver.exe"
+    solo_agentes = 0
+    no_dupli = 1
+    resp = os.getcwd()
+
+    #Vamos a llamar esto n veces teniendo en cuenta que siempre se para solo
+    for n in range(1):
+        feats = scrape_data_corotos(url,tipo_neg,2,driver,loads=0,solo_agentes=solo_agentes,no_dupli=no_dupli)
+
+print(feats)
