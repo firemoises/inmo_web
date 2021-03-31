c6539dd53a3ee81c3ffd7ca49f8a8bb5e7fe7545
moises_df <moisesintec@gmail.com>
2021-03-31 16:45:09 -0400

    probando python ssh

--- prueba_ssh.py
+++ prueba_ssh.py
@@ -1,9 +1,21 @@
+import paramiko
+from time import sleep
+import random
+
+def print_ssh(output):
+    stdin, stdout, stderr = output
+    stdout._set_mode("rb")
+    lines = stdout.readlines()
+    print(lines)
+
 host = "148.0.3.66"
 port = 22
 username = "moise"
 password = "Secundario@444"
 
-command = "dir"
+command = "cd Dropbox"
+command2 = "cd GitHub dropbox"
+command3 = "mkdir pruebassh"
 
 ssh = paramiko.SSHClient()
 
@@ -11,9 +23,14 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ssh.connect(host, port, username, password)
 
+channel = ssh.get_transport().open_session()
+channel.invoke_shell()
 
-stdin, stdout, stderr = ssh.exec_command(command)
+while channel.recv_ready():
+    channel.recv(9999)
 
-lines = stdout.readlines()
+channel.sendall(command)
+channel.sendall(command2)
+channel.sendall(command3)
 
-print(lines)
+print(channel.recv(9999))
